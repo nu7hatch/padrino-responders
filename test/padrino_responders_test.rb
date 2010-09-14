d3889d38a89b805c403368d2729494e6834c1e70
Kriss Kowalik <kriss.kowalik@gmail.com>
2010-09-14 03:35:06 +0200

    Removed some code from tests

--- test/padrino_responders_test.rb
+++ test/padrino_responders_test.rb
@@ -1,26 +1,3 @@
 require 'teststrap'
 
-class MockApp < Padrino::Application
-  include Padrino::Responders
-  
-  controllers :test do
-    provides :html, :js
-    
-    get :simple do
-      respond(@users = [:foo, :bar])
-    end 
-    
-    post :creator do 
-    end
-    
-    put :updater do 
-    end
-    
-    delete :destroyer do 
-    end
-  end
-end
-
-context "Padrino responders plugin" do 
-  subject {}
-end
+puts "I'm to lazy to write tests for it!"
